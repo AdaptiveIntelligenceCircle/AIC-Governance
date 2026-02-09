96d224201df4a90f761c26e48cbd23dd7e7371fc
ndtribk <ndtribk@gmail.com>
2026-02-09 12:26:04 +0700

    signal_router.cpp

---
+++ runtime/signal_router.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+#include <vector> 
+#include <string> 
+
+namespace aic :: runtime 
+{
+    vector<string> routeSignals(const vector<string> &raw)
+    {
+        return raw; 
+    }
+}
